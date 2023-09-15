bd99b63b9e1d194b520a25951f7c4d8b943ff191
yannclergue <73096566+yannclergue@users.noreply.github.com>
2023-09-15 18:55:42 +0200

    Installer un nouveau design
    J'étais pas satisfait avec le design actuelle,
    je suis partie sur les couleurs de mon CV et j'ai recommencé la landing
    page. Bon, y'a encore de l'amélioration possible, mais on y croit

--- js/js.js
+++ js/js.js
@@ -1,10 +1,42 @@
-function menu() {
-    document.querySelector(".background").classList.toggle("hide")
+const dynamicTest = document.querySelector(".texteDefil span")
+const words = ["Codeur", "Rédacteur", "Joueur"];
+
+let wordIndex = 0;
+let charIndex = 0;
+let isDeleting = false;
+
+const typeEffect = () => {
+    const currentWord = words[wordIndex];
+    const currentChar = currentWord.substring(0, charIndex)
+    dynamicTest.textContent = currentChar;
+    dynamicTest.classList.add("stop-blinking")
+    console.log(currentWord, currentChar)
+
+    if (!isDeleting && charIndex < currentWord.length) {
+        charIndex++;
+        setTimeout(typeEffect, 200)
+    } else if (isDeleting && charIndex > 0) {
+        charIndex--;
+        setTimeout(typeEffect, 100)
+    } else {
+        isDeleting = !isDeleting;
+        dynamicTest.classList.remove("stop-blinking")
+        wordIndex = !isDeleting ? (wordIndex + 1) % words.length : wordIndex;
+        setTimeout(typeEffect, 1200)
+    }
 }
 
-function page() {
-    document.querySelector(".background").classList.toggle("hide")
-}
+typeEffect();
 
-document.querySelector("#menuForeground").addEventListener("click", menu)
-document.querySelector("#menuBackground").addEventListener("click", page)
+
+// function menu() {
+//     document.querySelector(".background").classList.toggle("hide")
+// }
+
+// function page() {
+//     document.querySelector(".background").classList.toggle("hide")
+// }
+
+// document.querySelector("#menuForeground").addEventListener("click", menu)
+// document.querySelector("#menuBackground").addEventListener("click", page)
+
