5315d4c42be2ea1d658cbd4a4ec098d440f96b92
yannclergue <73096566+yannclergue@users.noreply.github.com>
2023-10-05 10:17:36 +0200

    Rajouter des animations à la landing page
    Les 2 boutons en bas de la présentation ont leur fléches qui bouge à
    l'infini, et les blocs retracant mon parcours arrivent avec une
    animation

--- js/js.js
+++ js/js.js
@@ -1,3 +1,17 @@
+const parcours = document.querySelector(".trigger")
+
+window.addEventListener("scroll", () => {
+    const {scrollTop, clientHeight} = document.documentElement;
+
+   const topElementToTopViewport = parcours.getBoundingClientRect().top
+
+   if(scrollTop > (scrollTop + topElementToTopViewport).toFixed()) {
+    document.querySelector(".animation1").classList.add("active")
+    document.querySelector(".animation2").classList.add("active")
+    document.querySelector(".animation3").classList.add("active")
+   }
+})
+
 function appear(){
     document.querySelector(".mobileBack").classList.toggle("hide")
 }
@@ -8,3 +22,6 @@ function hide(){
 
 document.querySelector(".mobileFor img").addEventListener("click", appear)
 document.querySelector(".mobileBackHead img").addEventListener("click", hide)
+
+
+
